4d74105748f0ee89c2f245b8f0423f6ccfffe4ae
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-08-03 00:20:04 -0400

    Update streamanswer.js

--- backend/controller/streamanswer.js
+++ backend/controller/streamanswer.js
@@ -4,6 +4,8 @@
  * @param {*} res The response object of express route
  * @param {*} token LLM token to be written to data
  */
+
+// TODO: fix streaming, send headers before sending response
 function streamAnswerGPT(res, token) {
     res.write(token, 'utf8', () => {
     });
