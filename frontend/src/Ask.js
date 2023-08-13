f69c0705a894ecbb3838cea528e8c8923582d709
dourian <dorcwork@gmail.com>
2023-08-13 16:31:46 -0400

    some ui changes
    Co-authored-by: Sarina Li <74844953+sarinali@users.noreply.github.com>
    Co-authored-by: Jacqueline Ho <jacquelineho426@gmail.com>

--- frontend/src/Ask.js
+++ frontend/src/Ask.js
@@ -1,7 +1,6 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 
 import { BackendContext } from "./BackendProvider";
-import Box from "@mui/material/Box";
 import { IoIosArrowDropleftCircle } from "react-icons/io";
 import { RiArrowUpCircleFill } from "react-icons/ri";
 import { BsThreeDots } from "react-icons/bs";
@@ -14,12 +13,26 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Ask({ isDark, setIsDark }) {
   const [inputValue, setInputValue] = useState("");
-  const { getAnswer, isLoading, disabledAsk, setDisabledAsk, getPrompts, prompts, setPrompts } =
-    useContext(BackendContext);
+  const {
+    getAnswer,
+    isLoading,
+    disabledAsk,
+    setDisabledAsk,
+    getPrompts,
+    prompts,
+    setPrompts,
+  } = useContext(BackendContext);
   const [conversationArr, setConversationArr] = useState([]);
   const [renderedConversation, setRenderedConversation] = useState([]);
+  const [isNew, setIsNew] = useState(true);
 
   const notify = () => toast("You cannot enter a blank question!");
+
+  const bottomEl = useRef(null);
+
+  const scrollToBottom = () => {
+    bottomEl?.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   // useEffect(() => {
   //   if (!prompts.length) {
@@ -32,18 +45,14 @@ export default function Ask({ isDark, setIsDark }) {
     conversationArr.forEach((item) => {
       if (item.type === "user") {
         temp.push(
-          <div className=" bg-slate-500 min-h-[150px]">
-            <p className="text-right w-full pl-24 pr-10 text-lg bg-red">
-              {item.text}
-            </p>
+          <div className="bg-[#FFFFFF] min-h-[150px] border border-gray-300">
+            <p className="text-right w-full pl-24 pr-10 text-lg">{item.text}</p>
           </div>
         );
       } else if (item.type === "bot") {
         temp.push(
-          <div className=" bg-slate-300 min-h-[150px]">
-            <p className="text-left w-full pl-24 pr-10 text-lg bg-red">
-              {item.text}
-            </p>
+          <div className=" bg-[#F7F7F8] min-h-[150px] border border-gray-300">
+            <p className="text-left w-full pl-24 pr-10 text-lg">{item.text}</p>
           </div>
         );
       }
@@ -57,10 +66,6 @@ export default function Ask({ isDark, setIsDark }) {
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
   };
 
   const queryQuestion = async (event) => {
@@ -83,28 +88,35 @@ export default function Ask({ isDark, setIsDark }) {
         new Conversation("bot", ans),
       ]);
       console.log(conversationArr);
-      
+      setIsNew(false);
     }
   };
 
+  useEffect(() => {
+    scrollToBottom();
+  }, [renderedConversation]);
+
   return (
     <div className="w-full h-[100vh]">
-      <div className="bg-[#101625] w-[80px] h-[100vh] transition-all duration-100 ease-in-out absolute z-10">
+      <div className="bg-[#101625] w-[80px] h-[100vh] transition-all duration-100 ease-in-out absolute z-10 place-items-between ">
         <button className="m-4 border-b-[0.5px]" onClick={() => navigate("/")}>
           <IoIosArrowDropleftCircle className="fill-white text-[30px] m-2" />
         </button>
-        <button className="flex items-center align-center w-full">
-          <BsThreeDots className="flex mx-auto mt-[85vh] fill-white text-[20px]" />
+        <button className="flex items-center align-center w-full mt-[85vh]">
+          <BsThreeDots className="flex mx-auto fill-white text-[20px]" />
         </button>
       </div>
       <div className="w-full h-[100vh] absolute flex flex-col justify-between z-0">
-        <h1 className="mt-[80px] text-6xl font-bold text-black text-center w-full">
-          UniGPT
-        </h1>
-        <div className=" h-5/6 overflow-scroll">
-        {renderedConversation}
+        <div className="drop_shadow w-full">
+          <h1 className="mt-[15px] mb-[15px] text-6xl font-bold text-black text-center w-full">
+            UniGPT
+          </h1>
         </div>
-        <Prompts />
+        <div className="overflow-scroll flex item-start flex-col h-full">
+          {renderedConversation}
+          <div ref={bottomEl}></div>
+        </div>
+        {isNew && <Prompts />}
         <div className="flex flex-row w-full mb-[10px] items-center justify-center gap-4">
           <form onSubmit={queryQuestion} className="w-[500px] drop_shadow">
             <label>
